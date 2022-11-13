6a17c69ba79c3ba42d898db49911ec033e46b4f7
Eric Petsopoulos <epetsopoulos@gmail.com>
2022-11-13 14:59:12 +0000

    feat: full screen mode init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import "./App.css";
 
 function App() {
+  const handle = useFullScreenHandle();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handle.enter}>Enter fullscreen</button>
+      <FullScreen handle={handle}>
+        <img
+          src="https://drive.google.com/uc?export=view&id=11AH-SHVgdUqLgmFSzCeEA5gHwSjoHeO0"
+          alt="prompt"
+        />
+      </FullScreen>
+    </>
   );
 }
 
